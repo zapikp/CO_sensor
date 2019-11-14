9933a109b3ba40101f09ed99f8bc39a675f12c50
zapikp <56720963+zapikp@users.noreply.github.com>
2019-11-14 10:22:44 +0100

    Delete password.h

--- src/password.h
+++
@@ -1,8 +0,0 @@
-#ifdef HELENA
-  const char* ssid     = "BLABLABLA";
-  const char* password = "6503292004";
-#else
-  const char* ssid     = "zapici";
-  //const char* ssid     = "AndroidAPzapik";
-  const char* password = "Jitka6063";
-#endif
